322caa7912b6f3e6f117b4e7567ddfc876bf84f5
David Schmitt <david@black.co.at>
2022-10-09 19:29:47 +0100

    start pre-fabbed command differentiation

--- src/data.rs
+++ src/data.rs
@@ -19,9 +19,9 @@ pub enum Target {
 
 /// A shell command to execute somewhere.
 #[derive(Clone)]
-pub struct Command {
-    pub title: String,
-    pub command: String,
+pub enum Command {
+    Shell { title: String, command: String },
+    Checkout,
 }
 
 /// A Job contains a bunch of Commands to run in sequence.
@@ -32,33 +32,34 @@ pub struct Job {
 }
 
 lazy_static! {
-    pub static ref INSTALL_OTEL_CLI: Command = Command {
+    pub static ref INSTALL_OTEL_CLI: Command = Command::Shell {
         title: "install otel-cli".into(),
         command: "curl -L https://github.com/equinix-labs/otel-cli/releases/download/v0.0.20/otel-cli-0.0.20-Linux-x86_64.tar.gz | sudo tar xvzf - -C /usr/local/bin".into(),
     };
     pub static ref CC_TEST: Job = Job {
         ecosystem: Ecosystem::Cargo,
         steps: vec![
-            Command {
+            Command::Shell {
                 title: "configure traceparent".into(),
                 command: "if [ -n \"$HOOK_URL\"]; then curl \"${HOOK_URL}/traceparent/${CIRCLE_WORKFLOW_ID}/${CIRCLE_WORKFLOW_JOB_ID}\" >> \"$BASH_ENV\"; fi".into(),
             },
             INSTALL_OTEL_CLI.clone(),
-            Command {
+            Command::Shell {
                 title: "install protoc".to_string(),
                 command: "sudo apt update && sudo apt install -y protobuf-compiler".to_string(),
             },
             // checkout
+            Command::Checkout,
             // restore cache
-            Command {
+            Command::Shell {
                 title: "cargo check".to_string(),
                 command: "cargo check --workspace".to_string(),
             },
-            Command {
+            Command::Shell {
                 title: "cargo test".to_string(),
                 command: "cargo test --workspace --doc".to_string(),
             },
-            Command {
+            Command::Shell {
                 title: "cargo build".to_string(),
                 command: "cargo build --workspace --release".to_string(),
             },
